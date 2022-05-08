c2250c53ccfc30e0a15dcddeae4fb180374f93c8
SaimaMeem <saimasultana1320@gmail.com>
2022-05-08 11:36:35 +0600

    get method for items done

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2fruj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const itemCollection = client.db('go4Fresh').collection('items');
+
+        //items api
+        //GET
+        app.get('/items', async (req, res) => {
+            const query = {};
+            const cursor = itemCollection.find(query);
+            const items = await cursor.toArray();
+            console.log(items);
+            res.send(items);
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir());
+
+app.get('/', async (req, res) => {
+    res.send('Running on the go4fresh server!');
+});
+
+
+
+app.listen(port, () => {
+    console.log('running on the port', port);
+})
+
