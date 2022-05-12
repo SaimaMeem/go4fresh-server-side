4eda82801bffba7cedc6c20e2b8e1e9df32d7755
SaimaMeem <saimasultana1320@gmail.com>
2022-05-12 07:24:27 +0600

    jwt implemented

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 require("dotenv").config();
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -9,14 +10,35 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 //middleware
 app.use(cors());
 app.use(express.json());
-
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: "Unauthorized Access" });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_SECRET_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: "Forbidden Access" });
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    });
+}
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2fruj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
         await client.connect();
         const itemCollection = client.db('go4Fresh').collection('items');
+        //AUTH
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const accessToken = jwt.sign(user, process.env.ACCESS_SECRET_TOKEN, { expiresIn: '1d' });
+            res.send({ accessToken });
 
+        })
         //items api
         //GET
         app.get('/items', async (req, res) => {
@@ -43,37 +65,43 @@ async function run() {
             const filter = { _id: ObjectId(id) };
             const options = { upsert: true };
             const updatedDoc = {
-                $set:{
+                $set: {
                     quantity: updatedItem.quantity,
                 }
             }
-            const result = await itemCollection.updateOne(filter,updatedDoc,options);
+            const result = await itemCollection.updateOne(filter, updatedDoc, options);
             res.send(result);
         })
 
         //DELETE DELETE ONE ITEM
-        app.delete('/items/:id', async (req,res)=>{
+        app.delete('/items/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id:ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await itemCollection.deleteOne(query);
             res.send(result);
         });
         //POST ADD ONE ITEM
-        app.post('/items',async(req,res)=>{
+        app.post('/items', async (req, res) => {
             const newIem = req.body;
             const result = await itemCollection.insertOne(newIem);
             console.log(result);
             res.send(result);
         });
-             //GET BASED ON EMAIL ADDRESS
-             app.get('/myitems', async (req, res) => {
-                 const email = req.query.email;
-                const query = {email:email};
-                const cursor = itemCollection.find(query);
-                const items = await cursor.toArray();
-                console.log(items);
-                res.send(items);
-            });
+        //GET BASED ON EMAIL ADDRESS
+        app.get('/myitems', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+           if(email === decodedEmail){
+            const query = { email: email };
+            const cursor = itemCollection.find(query);
+            const items = await cursor.toArray();
+            // console.log(items);
+            res.send(items);
+           }
+           else{
+               res.status(403).send({message:'Forbidden Access'})
+           }
+        });
     }
     finally {
 
